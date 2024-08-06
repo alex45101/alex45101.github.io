0e1c022288a7c78663dc209f16473252bd938af2
alex45101 <aleontiev00@gmail.com>
2024-08-05 23:14:00 -0700

    Working on input for cells while resizing

--- MatrixCalculator/MatrixCalc.js
+++ MatrixCalculator/MatrixCalc.js
@@ -9,20 +9,13 @@ class matrix {
         this.id = matrixCounter;
         this.initialSetup = true;
 
+        this.vertices = [];
+
         matrixCounter++;
     }
 
     setUpTable() {
-        let currDiv;
-        if (this.initialSetup) {
-            //generate a div and label it corresponding to the matrix id
-            currDiv = document.createElement("div");
-            currDiv.id = "matrixDiv-" + this.id.toString();
-        }
-        else {
-            //grab the current div for the matrix
-            currDiv = document.getElementById("matrixDiv-" + this.id.toString());
-        }
+        let currDiv = this.getCurrDiv();
 
         //generate a form and label it corresponding to the matrix id
         let form = document.createElement("form");
@@ -33,14 +26,20 @@ class matrix {
         table.id = "matrixTable-" + this.id.toString();
 
         //generate the cells for each position in the matrix
+        let newId = 0;
         for (var i = 0; i < this.length; i++) {
             let row = document.createElement("tr");
 
-            for (var j = 0; j < this.width; j++) {
-                let textBox = document.createElement("input");
+            for (var j = 0; j < this.width; j++, newId++) {
                 let cell = document.createElement("td");
-
+                
+                //CLEAN THIS UP
+                let textBox = document.createElement("input");
+                textBox.id = (newId).toString();
                 textBox.input = "text";
+                if (this.vertices[textBox.id] != undefined)
+                    textBox.value = this.vertices[textBox.id];
+                textBox.onchange = (event) => { this.vertices[textBox.id] = textBox.value; };
 
                 cell.appendChild(textBox);
                 row.appendChild(cell);
@@ -50,20 +49,11 @@ class matrix {
         form.appendChild(table);
 
         //Generate Increase and Decrease Buttons
-        let increaseButton = document.createElement("input");
-        increaseButton.id = "increaseButton-" + this.id.toString();
-        increaseButton.type = "button";
-        increaseButton.value = "+";
-        increaseButton.onclick = () => this.matrixResize(increaseButton);
+        var buttons = this.generateResizeButtons();
 
-        let decreaseButton = document.createElement("input");
-        decreaseButton.id = "decreaseButton-" + this.id.toString();
-        decreaseButton.type = "button";
-        decreaseButton.value = "-";
-        decreaseButton.onclick = () => this.matrixResize(decreaseButton);
-
-        form.appendChild(decreaseButton);
-        form.appendChild(increaseButton);
+        for (var i = 0; i < buttons.length; i++) {
+            form.appendChild(buttons[i]);
+        }
 
         currDiv.appendChild(form);
 
@@ -76,9 +66,43 @@ class matrix {
         this.initialSetup = false;
     }
 
+    getCurrDiv() {
+        let currDiv;
+
+        //if initial setup generate div for matrix else grab it
+        if (this.initialSetup) {
+            currDiv = document.createElement("div");
+            currDiv.id = "matrixDiv-" + this.id.toString();
+        }
+        else {
+            currDiv = document.getElementById("matrixDiv-" + this.id.toString());
+        }
+
+        return currDiv;
+    }
+
+    generateResizeButtons() {
+        //generate the 2 buttons needed to resize a matrix
+        let increaseButton = document.createElement("input");
+        increaseButton.id = "increaseButton-" + this.id.toString();
+        increaseButton.type = "button";
+        increaseButton.value = "+";
+        increaseButton.onclick = () => this.matrixResize(increaseButton);
+
+        let decreaseButton = document.createElement("input");
+        decreaseButton.id = "decreaseButton-" + this.id.toString();
+        decreaseButton.type = "button";
+        decreaseButton.value = "-";
+        decreaseButton.onclick = () => this.matrixResize(decreaseButton);
+
+        return [decreaseButton, increaseButton];
+    }
+
     matrixResize(button) {
+        //clear the table then regenerate it at the end
         this.clearTable();
 
+        //if decreasing make sure that either lenght or width go below minTableSize
         if (button.value == "-") {
             if (this.length > minTableSize && this.width > minTableSize) {
                 this.length--;
@@ -94,12 +118,19 @@ class matrix {
     }
 
     clearTable() {
-        document.getElementById("matrixDiv-" + this.id.toString()).removeChild(document.getElementById("matrixForm-" + this.id.toString()));
+        //remove the current form from the matrix div
+        this.getCurrDiv().removeChild(document.getElementById("matrixForm-" + this.id.toString()));
     }
 }
 
 var mainMatrix = new matrix(3, 3);
 
+var matrices = [new matrix(2, 2), new matrix(5, 2), new matrix(4, 4)];
+
 document.body.onload = () => {
     mainMatrix.setUpTable();
+
+    for (var i = 0; i < 3; i++) {
+        matrices[i].setUpTable();
+    }
 }
