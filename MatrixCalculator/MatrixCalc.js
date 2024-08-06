b428c2b992c824c93798ad62f8d8fce635c4f08d
alex45101 <aleontiev00@gmail.com>
2024-08-05 22:32:26 -0700

    Modular matrix generator

--- MatrixCalculator/MatrixCalc.js
+++ MatrixCalculator/MatrixCalc.js
@@ -1,59 +1,105 @@
 const minTableSize = 2;
 
+var matrixCounter = 0;
+
 class matrix {
     constructor(length, width) {
         this.length = length;
         this.width = width;
+        this.id = matrixCounter;
+        this.initialSetup = true;
+
+        matrixCounter++;
+    }
+
+    setUpTable() {
+        let currDiv;
+        if (this.initialSetup) {
+            //generate a div and label it corresponding to the matrix id
+            currDiv = document.createElement("div");
+            currDiv.id = "matrixDiv-" + this.id.toString();
+        }
+        else {
+            //grab the current div for the matrix
+            currDiv = document.getElementById("matrixDiv-" + this.id.toString());
+        }
+
+        //generate a form and label it corresponding to the matrix id
+        let form = document.createElement("form");
+        form.id = "matrixForm-" + this.id.toString();
+
+        //generate a table and label it corresponding to the matrix id
+        let table = document.createElement("table");
+        table.id = "matrixTable-" + this.id.toString();
+
+        //generate the cells for each position in the matrix
+        for (var i = 0; i < this.length; i++) {
+            let row = document.createElement("tr");
+
+            for (var j = 0; j < this.width; j++) {
+                let textBox = document.createElement("input");
+                let cell = document.createElement("td");
+
+                textBox.input = "text";
+
+                cell.appendChild(textBox);
+                row.appendChild(cell);
+            }
+            table.appendChild(row);
+        }
+        form.appendChild(table);
+
+        //Generate Increase and Decrease Buttons
+        let increaseButton = document.createElement("input");
+        increaseButton.id = "increaseButton-" + this.id.toString();
+        increaseButton.type = "button";
+        increaseButton.value = "+";
+        increaseButton.onclick = () => this.matrixResize(increaseButton);
+
+        let decreaseButton = document.createElement("input");
+        decreaseButton.id = "decreaseButton-" + this.id.toString();
+        decreaseButton.type = "button";
+        decreaseButton.value = "-";
+        decreaseButton.onclick = () => this.matrixResize(decreaseButton);
+
+        form.appendChild(decreaseButton);
+        form.appendChild(increaseButton);
+
+        currDiv.appendChild(form);
+
+        if (this.initialSetup) {
+            //get the div element for all the matrices and add the new matrix to it
+            let matricesDiv = document.getElementById("matrices");
+            matricesDiv.appendChild(currDiv);
+        }
+
+        this.initialSetup = false;
+    }
+
+    matrixResize(button) {
+        this.clearTable();
+
+        if (button.value == "-") {
+            if (this.length > minTableSize && this.width > minTableSize) {
+                this.length--;
+                this.width--;
+            }
+        }
+        else if (button.value == "+") {
+            this.length++;
+            this.width++;
+        }
+
+        this.setUpTable();
+    }
+
+    clearTable() {
+        document.getElementById("matrixDiv-" + this.id.toString()).removeChild(document.getElementById("matrixForm-" + this.id.toString()));
     }
 }
 
 var mainMatrix = new matrix(3, 3);
 
 document.body.onload = () => {
-    setUpTable();
-}
-
-function setUpTable() {
-    let matrixDiv = document.getElementById("mainMatrixTable");
-    let matrixTable = document.createElement("table");
-    matrixTable.id = "matrixTable";
-
-    for (var i = 0; i < mainMatrix.length; i++) {
-        let row = document.createElement("tr");
-
-        for (var j = 0; j < mainMatrix.width; j++) {
-            let textBox = document.createElement("input");
-            let cell = document.createElement("td");
-
-            textBox.input = "text";
-
-            cell.appendChild(textBox);
-            row.appendChild(cell);
-        }
-        matrixTable.appendChild(row);
-    }
-
-    matrixDiv.appendChild(matrixTable);
-}
-
-function clearTable() {
-    let matrix = document.getElementById("mainMatrixTable");
-    matrix.removeChild(document.getElementById("matrixTable"));
-}
-
-function matrixResize(button) {
-    clearTable();
-
-    if (button.value == "-") {
-        if (mainMatrix.length > minTableSize && mainMatrix.width > minTableSize) {
-            mainMatrix.length--;
-            mainMatrix.width--;
-        }
-    }
-    else if (button.value == "+") {
-        mainMatrix.length++;
-        mainMatrix.width++;
-    }
-
-    setUpTable();
+    mainMatrix.setUpTable();
 }
